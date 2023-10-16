8fb45ddab491341157b1f4eac05b396ba1b8e64f
Alessia Bellisario <github@bellisar.io>
2023-10-16 14:25:06 -0400

    chore: include reference path

--- src/testing/matchers/index.ts
+++ src/testing/matchers/index.ts
@@ -1,3 +1,5 @@
+/// <reference path="./index.d.ts" />
+
 import { expect } from "@jest/globals";
 import { toMatchDocument } from "./toMatchDocument.js";
 import { toHaveSuspenseCacheEntryUsing } from "./toHaveSuspenseCacheEntryUsing.js";
